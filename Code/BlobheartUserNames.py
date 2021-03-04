3f3a5fd7301c1900d1a3b59d881fdc29c4eb5054
Quark064 <wolfer473gmail.com>
2021-03-04 00:07:10 -0900

    Removed inactive usernames

--- Code/BlobheartUserNames.py
+++ Code/BlobheartUserNames.py
@@ -2,10 +2,8 @@ fortniteHandles = [
     'Wolfer473',
     'The Bellyman',
     'milkyhilky',
-    '5xj',
     'SkyBlueNinja42',
     'Gwem_',
     'tedi_spl',
-    'bisexual pride',
     'shovelpersona'
     ]
